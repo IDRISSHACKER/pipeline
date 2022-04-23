8c7d72a5f44f952959adf77cbb72e2f5c25868dc
IDRISSHACKER <idrisscoder@gmail.com>
2022-04-23 07:44:12 +0100

    Fin de l'ecriture des tests et du code

---
+++ src/interval.js
@@ -0,0 +1,29 @@
+const SECOND = 1000
+const MINUTE = 60 * SECOND
+const HOURS = 60 * MINUTE
+const DAYS = 24 * HOURS
+
+const times = {
+    days: DAYS,
+    hours: HOURS,
+    minutes: MINUTE
+}
+
+module.exports = function intervalToObj (ms) {
+    ms = Math.abs(ms)
+    const timer = {
+        days : 0,
+        hours : 0,
+        minutes : 0,
+        seconds : 0
+    } 
+    
+    Object.keys(times).forEach(key=>{
+        timer[key] = Math.floor(ms / times[key])
+        ms -= timer[key] * times[key]
+    })
+
+    timer.seconds = Math.round(ms / 1000)
+
+    return timer
+}
--- tests/interval.spec.js
+++ tests/interval.spec.js
@@ -1,8 +1,8 @@
-import {intervalToObj} from "../src/interval"
+const intervalToObj = require("../src/interval")
 
 test("shold return object for 0 second", ()=>{
 
-    const obj = intervalToObj(0)
+    const entry = intervalToObj(0)
     const objResult = {
         days: 0,
         hours: 0,
@@ -10,5 +10,109 @@ test("shold return object for 0 second", ()=>{
         seconds: 0
     }
 
-    describe(intervalToObj(0)).toEqual(objResult)
+    expect(entry).toEqual(objResult)
+})
+
+test("shold return object for 3 second", () => {
+
+    const entry = intervalToObj(3 * 1000)
+    const objResult = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 3
+    }
+
+    expect(entry).toEqual(objResult)
+})
+
+test("shold return object for 15 second", () => {
+
+    const entry = intervalToObj(15 * 1000)
+    const objResult = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 15
+    }
+
+    expect(entry).toEqual(objResult)
+})
+
+test("shold return object for 15 second 25ms", () => {
+
+    const entry = intervalToObj(15 * 1025)
+    const objResult = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 15
+    }
+
+    expect(entry).toEqual(objResult)
+})
+
+test("shold handle minutes", () => {
+
+    const entry = intervalToObj(3 * 60 * 1000 + 15 * 1025)
+    const objResult = {
+        days: 0,
+        hours: 0,
+        minutes: 3,
+        seconds: 15
+    }
+
+    expect(entry).toEqual(objResult)
+})
+
+test("shold handle  1 minutes", () => {
+
+    const entry = intervalToObj(60 * 1000)
+    const objResult = {
+        days: 0,
+        hours: 0,
+        minutes: 1,
+        seconds: 0
+    }
+
+    expect(entry).toEqual(objResult)
+})
+
+test("shold handle  3 hours", () => {
+
+    const entry = intervalToObj(10800000+1000*60)
+    const objResult = {
+        days: 0,
+        hours: 3,
+        minutes: 1,
+        seconds: 0
+    }
+
+    expect(entry).toEqual(objResult)
+})
+
+test("shold handle  15 days", () => {
+
+    const entry = intervalToObj(10800000 * 5 * 24 + 10800000 + 1000 * 60)
+    const objResult = {
+        days: 15,
+        hours: 3,
+        minutes: 1,
+        seconds: 0
+    }
+
+    expect(entry).toEqual(objResult)
+})
+
+test("shold handle  negative bumber", () => {
+
+    const entry = intervalToObj(-(10800000 * 5 * 24 + 10800000 + 1000 * 60))
+    const objResult = {
+        days: 15,
+        hours: 3,
+        minutes: 1,
+        seconds: 0
+    }
+
+    expect(entry).toEqual(objResult)
 })
